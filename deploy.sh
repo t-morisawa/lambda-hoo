e32692d1d9aab051238ae3415be41681bf7324ae
Toma Morisawa <13.35.104@gmail.com>
2019-01-02 22:04:29 +0900

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,35 +1,55 @@
 DIRNAME=lambda-hoo-`date +'%s'`
 DIRPATH=/tmp/$DIRNAME
 mkdir $DIRPATH
-cp -p *.py $DIRPATH
+cp -p src/* $DIRPATH
 cp -pr venv/lib/python3.6/site-packages/* $DIRPATH
-
-# ディレクトリへ移動
 pushd $DIRPATH
 zip -r ../$DIRNAME.zip ./*
 
-# Lambdaにアップロード
-# aws lambda create-function \
-#     --function-name lambda-hoo  \
-#     --region ap-northeast-1 \
-#     --zip-file fileb://../$DIRNAME.zip \
-#     --handler get_chiebukuro.handler \
-#     --runtime python3.6 \
-#     --timeout 60 \
-#     --role arn:aws:iam::047480778460:role/service-role/lambda-hoo-test \
-#     --memory-size 128
+# [FIXME] parameters
+AWSID=0123456789012
+ROLENAME=foo-role
+REGION=ap-northeast-1
+EVENTNAME=foo-event
+FUNCTIONNAME=foo-function
 
-# Lambdaにアップロード
-aws lambda update-function-code \
-    --function-name lambda-hoo \
+# create lambda exec roles
+aws iam create-role --role-name $ROLENAME --assume-role-policy-document '{"Version":"2012-10-17","Statement":[{"Effect":"Allow","Principal":{"Service":"lambda.amazonaws.com"},"Action":"sts:AssumeRole"}]}'
+
+# wait for avoid errors below create-function
+sleep 10
+
+# upload to Lambda (only first time)
+aws lambda create-function \
+    --function-name $FUNCTIONNAME  \
+    --region $REGION \
     --zip-file fileb://../$DIRNAME.zip \
-    --publish
+    --handler slack-post.handler \
+    --runtime python3.6 \
+    --timeout 60 \
+    --role arn:aws:iam::$AWSID:role/$ROLENAME \
+    --memory-size 128
 
+# upload to Lambda (after the 2nd time)
+# aws lambda update-function-code \
+    #     --function-name $FUNCTIONNAME \
+    #     --zip-file fileb://../$DIRNAME.zip \
+    #     --publish
+
+# [START] set periodic execution
 aws events put-rule \
-    --name lambda-hoo-scheduled \
-    --schedule-expression 'cron(0/15 * * * ? *)'
+    --name $EVENTNAME \
+    --schedule-expression 'cron(0/5 * * * ? *)'
+# --schedule-expression 'cron(55 9 * * ? *)'
 
-aws events put-targets --rule lambda-hoo-scheduled --targets "Id"="1","Arn"="arn:aws:lambda:ap-northeast-1:047480778460:function:lambda-hoo"
+aws lambda add-permission \
+    --function-name $FUNCTIONNAME \
+    --statement-id $EVENTNAME \
+    --action 'lambda:InvokeFunction' \
+    --principal events.amazonaws.com \
+    --source-arn arn:aws:events:$REGION:$AWSID:rule/$EVENTNAME
 
-# 元のディレクトリに戻る
+aws events put-targets --rule $EVENTNAME --targets "Id"="1","Arn"="arn:aws:lambda:$REGION:$AWSID:function:$FUNCTIONNAME"
+# [END] set periodic execution
+
 pushd
